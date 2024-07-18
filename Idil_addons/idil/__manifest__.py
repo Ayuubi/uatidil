620d78da10be23b249933530fa21e14323dd864a
Ayuubi <164630299+Ayuubi@users.noreply.github.com>
2024-07-18 14:35:18 +0300

    Update __manifest__.py
    Correcting Delete button

--- Idil_addons/idil/__manifest__.py
+++ Idil_addons/idil/__manifest__.py
@@ -7,7 +7,7 @@
     'depends': ['mail', 'point_of_sale', 'web'],
     'application': True,
     'sequence': -100,
-    'author': 'protyping',
+    'author': 'MDC',
 
     'assets': {
         'web.assets_common': [
