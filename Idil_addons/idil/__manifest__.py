925df20e52b62cb58530c9eb96a811909e1e2768
Ayuubi <164630299+Ayuubi@users.noreply.github.com>
2024-07-22 15:47:44 +0300

    Update __manifest__.py
    colortest

--- Idil_addons/idil/__manifest__.py
+++ Idil_addons/idil/__manifest__.py
@@ -7,7 +7,7 @@
     'depends': ['mail', 'point_of_sale', 'web'],
     'application': True,
     'sequence': -100,
-    'author': 'color',
+    'author': 'colortest',
 
     'assets': {
         'web.assets_common': [
