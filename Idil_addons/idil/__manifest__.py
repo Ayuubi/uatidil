79e0d1ec7b0b1de9710b6c107fe687051ad75484
Ayuubi <164630299+Ayuubi@users.noreply.github.com>
2024-07-22 15:26:49 +0300

    Update __manifest__.py
    color change

--- Idil_addons/idil/__manifest__.py
+++ Idil_addons/idil/__manifest__.py
@@ -7,7 +7,7 @@
     'depends': ['mail', 'point_of_sale', 'web'],
     'application': True,
     'sequence': -100,
-    'author': 'pull',
+    'author': 'color',
 
     'assets': {
         'web.assets_common': [
