83e9882ae8d7041e2195e5515df8ca46f7e073d9
Ayuubi <164630299+Ayuubi@users.noreply.github.com>
2024-08-13 15:02:40 +0300

    Update __manifest__.py
    alot files were changed before this commit

--- Idil_addons/idil/__manifest__.py
+++ Idil_addons/idil/__manifest__.py
@@ -7,7 +7,7 @@
     'depends': ['mail', 'point_of_sale', 'web'],
     'application': True,
     'sequence': -100,
-    'author': 'cadani',
+    'author': 'alot',
 
     'assets': {
         'web.assets_common': [
