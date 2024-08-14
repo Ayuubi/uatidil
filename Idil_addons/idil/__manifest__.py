a5c41438788e906e1a60f162019e4e6672acbad3
Ayuubi <164630299+Ayuubi@users.noreply.github.com>
2024-08-14 13:06:48 +0300

    Update __manifest__.py
    access list hiding

--- Idil_addons/idil/__manifest__.py
+++ Idil_addons/idil/__manifest__.py
@@ -7,7 +7,7 @@
     'depends': ['mail', 'point_of_sale', 'web'],
     'application': True,
     'sequence': -100,
-    'author': 'alot',
+    'author': 'access',
 
     'assets': {
         'web.assets_common': [
