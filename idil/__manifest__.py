a8dd0ccbde51c83ed4192845ec71b1fbd6de0f8f
Ayuubi <164630299+Ayuubi@users.noreply.github.com>
2024-10-15 05:38:28 -0700

    Update __manifest__.py
    triggering the change

--- idil/__manifest__.py
+++ idil/__manifest__.py
@@ -7,7 +7,7 @@
     'depends': ['mail', 'point_of_sale', 'web'],
     'application': True,
     'sequence': -100,
-    'author': 'access',
+    'author': 'MDC',
 
     'assets': {
         'web.assets_common': [
